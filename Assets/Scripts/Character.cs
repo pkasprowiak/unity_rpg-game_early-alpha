7881483b6763d2bc5027e5e2f17b19eeb7d90fa9
Pawel <kasprowiak.pawel@gmail.com>
2018-12-18 20:00:56 +0100

    First Commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    protected Vector2 direction;
+    private Animator animator;
+
+    // Use this for initialization
+    protected virtual void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update ()
+    {
+        //print("character");
+        Move();	
+	}
+
+    public void Move()
+    {
+        if( direction.x != 0 || direction.y != 0)
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+            AnimateMovement(direction);
+        }
+        else animator.SetLayerWeight(1, 0);
+    }
+
+    public void AnimateMovement(Vector2 direction)
+    {
+        animator.SetLayerWeight(1, 1);
+
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+	// Use this for initialization
+	protected override void Start ()
+    {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update ()
+    {
+        //print("Kruci gang");
+        //print("x:"+direction.x);
+        print("y:"+direction.y);
+        GetInput();
+        base.Update();
+	}
+
+
+
+    private void GetInput()
+    {
+        
+        direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+
+    }
+}
